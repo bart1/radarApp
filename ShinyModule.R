c125135387ac921cafbb8a36f028fb261a6a08a8
Bart <bart@myotis>
2025-05-05 18:11:51 +0200

    update

--- ShinyModule.R
+++ ShinyModule.R
@@ -2,14 +2,30 @@ library("shiny")
 library("shinydashboard")
 library("move2")
 library("moveShiny")
-future::plan(future::cluster)
+cl <- parallel::makeCluster(6, timeout = 60)
+
+future::plan(future::cluster, workers=cl)
+
+parallel::clusterEvalQ(cl,library(bioRad))
+parallel::clusterEvalQ(cl,library(memoise))
+parallel::clusterEvalQ(cl,library(shiny))
+parallel::clusterEvalQ(cl,library(starsTileServer))
+
+
 #devtools::load_all("~/moveShiny")
 #library("moveShiny")
 
 library("stringr")
 library("sf")
 library("leaflet")
-shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "radarAppCache")))
+#shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "radarAppCache")))
+shinyOptions(cache =cc<- cachem::cache_mem(logfile=stdout()))
+cc<-cachem::cache_disk()
+parallel::clusterExport(cl, c("cc"))
+parallel::clusterEvalQ(cl, shinyOptions(cache=cc))
+#parallel::clusterEvalQ(cl,    transform_matrix_cache2 <- memoise::memoise(compiler::cmpfun(moveShiny::transform_matrix), cache = shiny::getShinyOption("cache")))
+
+#shinyOptions(cache = cachem::cache_mem())
 
 # to display messages to the user in the log file of the App in MoveApps
 # one can use the function from the src/common/logger.R file:
@@ -25,7 +41,17 @@ shinyModuleUserInterface <- function(id, label) {
 
   tagList(
     dashboardPage(
-      dashboardHeader(title = "Radar track explore"),
+      dashboardHeader(title = "Radar track explore",
+                        dropdownMenu(type = "messages", badgeStatus =NULL# "success"
+                                     , icon = icon("gear"),
+                                     headerText = "",
+                                   messageItem("Track visualization",
+                                               sliderInput(min = 0, max = 120,inputId = ns("asdf"),label = "Tail duration (minutes)", value = 20, step = 1)
+                                   ),
+                                   messageItem("Radar display",
+                                               "This is the content of another message."
+                                   )
+                      )),
       dashboardSidebar(
         moveDateSelectUI(ns("dateRangeNS")),
         moveMapTimeSliderUI(ns("timeSliderNS")),
@@ -41,24 +67,42 @@ shinyModuleUserInterface <- function(id, label) {
 
 # The parameter "data" is reserved for the data object passed on from the previous app
 shinyModule <- function(input, output, session, data) {
-  # # all IDs of UI functions need to be wrapped in ns()
+  # all IDs of UI functions need to be wrapped in ns()
   ns <- session$ns
   output$prt <- renderPrint({
     invalidateLater(500)
-    list(future::availableCores(), time$time(), format(Sys.time(), "%H:%M:%OS3"))
+    list(future::availableCores(), time$time(), format(Sys.time(), "%H:%M:%OS3"), radar$scanTime())
   })
   output$map <- renderLeaflet(leaflet(height = "600px") |> addTiles())
-  map_proxy <- (leafletProxy(("map")) |> setView(5, 52, 5))
-  # note the map proxy needs to be defined here to avoid using the wrong id
+  map_proxy <- leafletProxy("map") |> setView(15, 62, 5)
+  colFun<-reactive({colorFactor("RdYlBu", levels=unique(mt_track_id(current())))})
   time <- moveMapTimeSliderServer("timeSliderNS",
     map_proxy = map_proxy,
-    move = dateSelected$move
+    move = dateSelected$move,
+    colorFunctionTrackId = colFun,
+    updateTime=radar$scanTime |> throttle(1500),tailDurationMinutes =reactive({input$asdf})
   )
-  radarMapServer("radarMapNS",
+
+  radar<-radarMapServer("radarMapNS",
     map_proxy = map_proxy,
     time = time$time
   )
   dateSelected <- moveDateSelectServer("dateRangeNS", current)
+  # moveRadarTime<-reactive({
+  #   req(radar$scanTime())
+  #   req(dateSelected$move())
+  #   m<-dateSelected$move()
+  #
+  #   mm<-move2::mt_interpolate(m, time = radar$scanTime(), omit = T)
+  #   mm<-mm[!sf::st_is_empty(mm),]
+  #   mm$event_id<-1:nrow(mm)
+  #   mm
+  # })
+  # moveMapPointsServer("pointsModule",moveRadarTime,
+  #                     map_bounds = reactive(input$map_bounds),
+  #                     map_marker_click = reactive(input$map_marker_click),
+  #                     map_proxy = map_proxy)
+  # moveMapLinesServer(map_proxy = map_proxy, id="linesNS", move = moveLastHour)
   #d <- as.Date("2023-6-30")
 
   #     current<-reactiveVal({
@@ -87,16 +131,16 @@ shinyModule <- function(input, output, session, data) {
   #     )
   # })
 
-#  current <- reactiveVal({
-#    m <-
-#      move2::mt_stack(
-#        move2::movebank_download_study(1415844328,
-#          attributes = NULL,
-#          timestamp_start = as.POSIXct("2020-10-1"),
-#          timestamp_end = as.POSIXct("2020-10-20")
-#        )
-#      )
-#  })
+ # current <- reactiveVal({
+ #   m <-
+ #     move2::mt_stack(
+ #       move2::movebank_download_study(1415844328,
+ #         attributes = NULL,
+ #         timestamp_start = as.POSIXct("2020-10-10"),
+ #         timestamp_end = as.POSIXct("2020-10-16")
+ #       )
+ #     )
+ # })
 
    current <- reactiveVal(data)
   #
